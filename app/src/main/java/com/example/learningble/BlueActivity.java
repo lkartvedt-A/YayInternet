a99c2f2cc9a223c82554a3ba5d6c0607f4347296
Lindsey <lindsey.kartvedt@allegion.com>
2019-06-13 13:52:51 -0600

    first real commit

---
+++ app/src/main/java/com/example/learningble/BlueActivity.java
@@ -0,0 +1,27 @@
+package com.example.learningble;
+
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            finish();
+        }
+    }
+}
