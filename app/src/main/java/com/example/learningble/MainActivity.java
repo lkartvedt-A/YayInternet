9e0f3200b3fe12114278b66770deeed26d4609bd
Lindsey <lindsey.kartvedt@allegion.com>
2019-06-21 15:59:51 -0600

    buttons created

--- app/src/main/java/com/example/learningble/MainActivity.java
+++ app/src/main/java/com/example/learningble/MainActivity.java
@@ -1,35 +1,48 @@
 package com.example.learningble;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
-public class MainActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.post_button).setOnClickListener(this);
+        findViewById(R.id.get_button).setOnClickListener(this);
+        findViewById(R.id.put_button).setOnClickListener(this);
+        findViewById(R.id.patch_button).setOnClickListener(this);
+        findViewById(R.id.delete_button).setOnClickListener(this);
+        Log.d("BUTTONS:", "onCLickListener Set!");
+
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+    @Override
+    public void onClick(View v){
+
+        switch(v.getId()) {
+            case R.id.post_button:
+                Log.d("POST BUTTON:", "Works!");
+                break;
+            case R.id.get_button:
+                Log.d("GET BUTTON:", "Works!");
+                break;
+            case R.id.put_button:
+                Log.d("PUT BUTTON:", "Works!");
+                break;
+            case R.id.patch_button:
+                Log.d("PATCH BUTTON:", "Works!");
+                break;
+            case R.id.delete_button:
+                Log.d("DELETE BUTTON:", "Works!");
+                break;
+        }
+
     }
 
-    /** Called when the user taps the Send button */
-    public void startBlue(View view) {
-        Intent intent = new Intent(this, BlueActivity.class);
-        startActivity(intent);
-    }
 }
